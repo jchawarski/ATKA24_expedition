8b327a83ab4ebf81ce8c91d38b8376dbcb9adf40
Julian Chawarski <jchawarski@aslenv.net>
2024-10-25 14:06:08 -0700

    algo nearly completed, needs TVG range correction applied and dataflow needs cleaning

--- ATKA24_CTD.R
+++ ATKA24_CTD.R
@@ -23,7 +23,11 @@ require(readxl)
 setwd("C:/Users/jchawarski/OneDrive - ASL Environmental Sciences Inc/Projects/Atka Expedition - SW Greenland/ATKA24 DATA")
 # single file reading and stripping of variables
 
-stn1 <- read_excel("RBR_CTD_Tu/ATKA24_01_CTD3_full.xlsx", sheet=4, skip=1)
+
+#### Create a heave file from CTD - for use in Echoview only ####
+
+
+stn1 <- read_excel("RBR_CTD_Tu/Exported_full_casts/ATKA24_01_CTD3_full.xlsx", sheet=4, skip=1)
 
 stn1$Depth_date <- format(as.Date(stn1$Time, format = "%Y-%m-%d"), "%m/%d/%Y" )
 stn1$Depth_time <- format(stn1$Time, format = "%H:%M:%S")
@@ -33,6 +37,7 @@ stn1 <- stn1[12:15]
 
 write.csv(stn1, "ATKA24_01_CTD3_heave.depth.csv")
 
+#### AZFP DATA PROCESSING PIPELINE ####
 
 # read AZFP data as sv.csv exported from AZFP link
 
@@ -48,7 +53,6 @@ colnames(nano.meta) <- nano[1,1:6]         # assign the headers to the metadata
 nano.meta$Date<- as.Date(as.character(nano.meta$Ping_date), "%Y-%m-%d")
 nano.meta$datetime <- as.POSIXct(paste(nano.meta$Ping_date, nano.meta$Ping_time), format="%Y-%m-%d %H:%M:%S", tz="UTC")
 
-
 # 2) Read in accompanying CTD file
 
 stn1 <- read_excel("RBR_CTD_Tu/Exported_full_casts/ATKA24_01_CTD1_full.xlsx", sheet=5, skip=1)
@@ -59,11 +63,11 @@ stn1$datetime <- as.POSIXct(stn1$Time,format="%Y-%m-%d %H:%M:%S", tz="UTC")
 
 stn1$interval <- period_to_seconds(lubridate::seconds(stn1$datetime))
 
-# calculate 1 second averages for the CTD data and round the interval to nearest second
+# 3) Create matching CTD-AZFP file - 
+#     calculate 1 second averages for the CTD data and round the interval to nearest second
 
 stn.sum <- 
-
-stn1 %>% group_by(round(interval)) %>% summarise(depth = mean(Depth),
+  stn1 %>% group_by(round(interval)) %>% summarise(depth = mean(Depth),
                                           sound_speed = mean(`Speed of sound`),
                                           conductivity = mean(Conductivity),
                                           salinity = mean(Salinity),
@@ -73,14 +77,17 @@ stn1 %>% group_by(round(interval)) %>% summarise(depth = mean(Depth),
 colnames(stn.sum)[1] <- "interval"
 stn.sum$datetime <- as.POSIXct(stn.sum$interval ,origin = "1970-01-01",tz = "UTC")
 
-# calculate mean temperature, salinity, pressure to a range of 50 m below probe
-# define function to calculate rolling mean for a given window size
-# accurate sounds speed adjustments 
-rolling_mean <- function(x, depth, window = 50) {
-  sapply(1:length(x), function(i) {
-    # Define the range for averaging (depth[i] to depth[i] + window)
-    lower_bound <- depth[i]
-    upper_bound <- lower_bound + window
+# 4) Calculate 50m measurements for AZFP calibration
+
+  # calculate mean temperature, salinity, pressure to a range of 50 m below probe
+  # define function to calculate rolling mean for a given window size
+  # accurate sounds speed adjustments 
+  rolling_mean <- function(x, depth, window = 50) 
+    {
+    sapply(1:length(x), function(i) {
+      # Define the range for averaging (depth[i] to depth[i] + window)
+      lower_bound <- depth[i]
+      upper_bound <- lower_bound + window
     
     # Get the values within this depth range
     idx <- which(depth >= lower_bound & depth <= upper_bound)
@@ -98,26 +105,23 @@ stn.sum$salinity_50m_mean <- rolling_mean(stn.sum$salinity, stn.sum$depth)
 stn.sum$soundspeed_50m_mean <- rolling_mean(stn.sum$sound_speed, stn.sum$depth)
 stn.sum$pressure_50m_mean <- rolling_mean(stn.sum$pressure, stn.sum$depth)
 
-# Assign constants for calculating a rolling absorption coefficient
-# 200 kHz quad transducer
-c <-1450.5     # original sound speed
-coeff_abs <- 0.0230 # original coefficient of absorption
-t <- 0.003        # pulse duration (s)
-y <- 1.071000006050e-02 # steridians BP from XML file - two-way beam angle  --- Adjust based on transducer 
-f <- 200000 # frequency in Hz
-
-
-
-# calculate the absorption coefficient for each 50 m depth interval
-require(oce)
-coeff_abs_new <-swSoundAbsorption(frequency= f,    # new absorption coefficient
-                                  salinity = stn.sum$salinity_50m_mean,
-                                  temperature = stn.sum$temperature_50m_mean,
-                                  pressure = stn.sum$pressure_50m_mean, 
-                                  pH =8,
-                                  formulation = "francois-garrison") 
-
-stn.sum$coeff_abs_50m <- coeff_abs_new
+# 5) # Assign constants for calculating a rolling absorption coefficient
+      # 200 kHz quad transducer
+      c <-1450.5     # original sound speed
+      coeff_abs <- 0.0230 # original coefficient of absorption
+      t <- 0.003        # pulse duration (s)
+      y <- 1.071000006050e-02 # steridians BP from XML file - two-way beam angle  --- Adjust based on transducer 
+      f <- 200000 # frequency in Hz
+      # calculate the absorption coefficient for each 50 m depth interval
+      require(oce)
+      coeff_abs_new <-swSoundAbsorption(frequency= f,    # new absorption coefficient
+                                        salinity = stn.sum$salinity_50m_mean,
+                                        temperature = stn.sum$temperature_50m_mean,
+                                        pressure = stn.sum$pressure_50m_mean, 
+                                        pH =8,
+                                        formulation = "francois-garrison") 
+      # assign new absorption coefficient
+      stn.sum$coeff_abs_50m <- coeff_abs_new
 
 
 # plots full temperature profile (downcast and upcast)
@@ -147,6 +151,10 @@ nano.meta$interval <- period_to_seconds(lubridate::seconds(nano.meta$datetime))
 nano.ctd <- nano.meta %>% 
   left_join(., stn.sum, by="interval")
 
+#assign sound speed from CTD data
+c_new <- nano.ctd$soundspeed_50m_mean
+
+
 # Constants for Sv calculation:          - most of these are not needed as they fall under the 'power' terms
 # psi <- 10*log10(1.071000006050e-02) # dB re 1 Sr
 #tvr <- 1.698999938965e+02 # transmit voltage response, dB re 1uPa/Volt at 1 m, read as TVR 
@@ -156,7 +164,7 @@ nano.ctd <- nano.meta %>%
 
 
 #equi_beam_angle <- 10^((y+20*log10(f_nominal/f))/10)     # this is the previous equation using a broadband transducer   
-equi_beam_angle <- 10*log10(y)
+#equi_beam_angle <- 10*log10(y)
 
 
 # Isolate the Sv matrix from the exported file
@@ -175,31 +183,28 @@ Sample_count <- as.numeric(nano.meta$Sample_count)[1]
 nano.range <- sapply(1:dim(nano.sv)[2], function(i) Range_start + ((Range_stop-Range_start)/Sample_count)*(i+0.5))    
 nano.range <- matrix(nano.range, nrow=dim(nano.sv)[1], ncol = dim(nano.sv)[2], byrow = T) # creates the full range matrix
 
-# TVG Range Correction
-# Need to include a TVG range correction
-# According to Echoview half a pulse length is appropriate, confirm with Steve
-# Estimate correction offset value
-
-  Fs <- 64000 # Hz, digitization rate, or the frequency at which the analog-to-digital converter is sampling
-  samperf <-  1/Fs # microseconds between samples
-  TVG_corr <- (t/samperf)*(1/2)  # samples pulse length divided by samples, divided by two
-  
-# how do I apply this value to range???
-  
+        #  TVG Range Correction - Need to figure out how to apply this to range measurements
+        #  Need to include a TVG range correction
+        #  According to Echoview half a pulse length is appropriate, confirm with Steve
+        #  Estimate correction offset value
+        
+        #  Fs <- 64000 # Hz, digitization rate, or the frequency at which the analog-to-digital converter is sampling
+        #  samperf <-  1/Fs # microseconds between samples
+        #  TVG_corr <- (t/samperf)*(1/2)  # samples pulse length divided by samples, divided by two
 
 # calculate time matrix - this uses a constant sounds speed
 # calculates the time for the signal to return based on the pulse length and nominal sound speed
 time.mat <- apply(nano.range,1:2, function(i)(c*t/4+i)*2/c) 
-#assign sound speed from CTD data
-c_new <- nano.ctd$soundspeed_50m_mean
 
+# Range correction based on sound speed
 # calculate a new range matrix - this should use a variable sound speed as the probe is moving. range needs to be adjusted per ping
 # the per-ping adjustment to sounds speed is only valid for the portion of the profile where we have measured sound speed
 # should trim the acoustic profile ahead of time to avoid create NA values?
 nano.range_new <- apply(time.mat, 2, function(i) i*c_new/2-c_new*t/4)   #new range matrix  - solving for range (R) in 20logR-2aR-10log(c*t*)
-
-nano.range_new[1000:1005,1:5] # double check that dynamic sound speed results in a dynamic range result
-
+    # test to make sure it worked 
+    hist(nano.range_new[,4411])   # histogram of maximum ranges
+    nano.range_new[1000:1005,1:5] # subset
+    summary(nano.range_new[,4411]) # includes NAs for pings without CTD measurements
 
 
 # calculate nominal power matrix - isolates the power terms, removing any variables that include impacted by sounds speed. 
@@ -214,12 +219,17 @@ power[1000:1005,1:10]
   # absorption loss = 2ar = 2*nano.ctd$coeff_abs_50m*nano.range_new -- this is a vector that uses absorption calculated out to 50 m from transducer
     abs_loss <- 2*nano.ctd$coeff_abs_50m*nano.range_new 
   # volume reverberation coefficient = 10log10(c*t*psi/2) = 10*log10(c_new*t*y/2)
-    reverb_coeff <- 10*log10(c_new*t*y/2)  # THIS NEEDS TO BE RECALCULATED TO ADJUST THE TWO WAY BEAM ANGLE FOR EACH C_NEW VALUE (see Demer et al 2015)
+    # first calculate the adjusted beam angle
+    y_adj <- y*(c/c_new)^2 # from Demer et al (2015) recommendation
+    reverb_coeff <- 10*log10(c_new*t*y_adj/2)  
+    reverb_coeff <- matrix(reverb_coeff, nrow = nrow(power), ncol = ncol(power), byrow = TRUE)
 
 #Sv_new <- power+20*log10(nano.range_new) + 2*coeff_abs_new*nano.range_new  # -10*log10(c_new*t*equi_beam_angle/2) 
 
-# COMPUTATIONALLLY INTENSIVE - not working might need to rework into invidivual steps
-Sv_new <- apply(power, 2, function(i) i+spread_loss+abs_loss-reverb_coeff) # subtract the final term from the rest of the calculation
+# COMPUTATIONALLLY INTENSIVE - not working might need to rework into invdividual steps
+#Sv_new <- apply(power, 2, function(i) i+spread_loss+abs_loss-reverb_coeff) # subtract the final term from the rest of the calculation
+
+Sv_new <- power + spread_loss + abs_loss - reverb_coeff
 
 
 # add a calibration offset to all Sv values to account for system bandwidth effects for volume backscatter
@@ -227,11 +237,17 @@ Sv_new <- apply(power, 2, function(i) i+spread_loss+abs_loss-reverb_coeff) # sub
 Sv_offset <- 1.2 # dB --- this value is for a 300 microsecond pulse length at 200 kHz
 
 #add the Sv offset to the entire Sv matrix prior to calculating means
-Sv_new <- apply(Sv_new, 2, function(i) i-Sv_offset) # not tested
+Sv_new <- apply(Sv_new, 2, function(i) i+Sv_offset) # not tested
 
 
-# view data and make any adjustments
+## plot data
 
+# compute mean values 
+Sv_new_mean <- t(data.frame(apply(Sv_new, 1, function(i) log10(mean(10^i, na.rm=T)))))
+Sv_old_mean <- t(data.frame(apply(nano.sv, 1, function(i) log10(mean(10^i, na.rm=T)))))
+
+plot(Sv_new_mean[1,700:1300], type="l")
+lines(Sv_old_mean[1,700:1300], type = "l", col = "red")
 
 
 # adjust Sv using new sound speed and absorption coefficients
